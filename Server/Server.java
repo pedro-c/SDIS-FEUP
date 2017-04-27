22d42bbd9c70751869924a5ffc2a52284bea6f63
Pedro Costa <pedrodc23@gmail.com>
2017-04-27 15:52:35 +0100

    Project Structure

--- Server/Server.java
+++ Server/Server.java
@@ -15,4 +15,15 @@ public class Server {
 
         //message handler
     }
+
+    public static void main(String[] args) {
+        //For now lets receive a port
+        if(args.length != 1){
+            throw new IllegalArgumentException("\\nUsage: java server.ServerLauncher<port>*");
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server server= new Server(port);
+    }
 }
