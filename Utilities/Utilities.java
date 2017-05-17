0577abd3ea5ed59f9a319816584510ddd9cee59f
Pedro Costa <pedrodc23@gmail.com>
2017-05-17 22:03:55 +0100

    Object encryption and decryption functions

--- Utilities/Utilities.java
+++ Utilities/Utilities.java
@@ -1,8 +1,13 @@
 package Utilities;
 
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import java.io.*;
 import java.math.BigInteger;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
 
 public class Utilities {
@@ -45,4 +50,53 @@ public class Utilities {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         return timestamp.getTime();
     }
+
+    /**
+     * Object encryption and decryption    code from: https://codereview.stackexchange.com/a/66931
+     */
+
+    private static final String transformation = "AES/ECB/PKCS5Padding";
+
+    public static void encrypt(Serializable object, OutputStream ostream, String password) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        try {
+            byte[] key = password.getBytes();
+            // Length is 16 byte
+            SecretKeySpec sks = new SecretKeySpec(key, transformation);
+
+            // Create cipher
+            Cipher cipher = Cipher.getInstance(transformation);
+            cipher.init(Cipher.ENCRYPT_MODE, sks);
+            SealedObject sealedObject = new SealedObject(object, cipher);
+
+            // Wrap the output stream
+            CipherOutputStream cos = new CipherOutputStream(ostream, cipher);
+            ObjectOutputStream outputStream = new ObjectOutputStream(cos);
+            outputStream.writeObject(sealedObject);
+            outputStream.close();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Object decrypt(InputStream istream, String password) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+
+        byte[] key = password.getBytes();
+
+        SecretKeySpec sks = new SecretKeySpec(key, transformation);
+        Cipher cipher = Cipher.getInstance(transformation);
+        cipher.init(Cipher.DECRYPT_MODE, sks);
+
+        CipherInputStream cipherInputStream = new CipherInputStream(istream, cipher);
+        ObjectInputStream inputStream = new ObjectInputStream(cipherInputStream);
+        SealedObject sealedObject;
+        try {
+            sealedObject = (SealedObject) inputStream.readObject();
+            return sealedObject.getObject(cipher);
+        } catch (ClassNotFoundException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 }
